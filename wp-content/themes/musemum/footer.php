6ee67046fe75dbddab589cd09c0c68493f66bcb6
kaiyu li <krisfk@gmail.com>
2022-05-21 05:39:19 +0800

    .

--- wp-content/themes/musemum/footer.php
+++ wp-content/themes/musemum/footer.php
@@ -30,9 +30,9 @@
 
 
         <?php
-			if ( function_exists( 'the_privacy_policy_link' ) ) {
-				the_privacy_policy_link( '<div class="privacy-policy">', '</div>' );
-			}
+			// if ( function_exists( 'the_privacy_policy_link' ) ) {
+			// 	the_privacy_policy_link( '<div class="privacy-policy">', '</div>' );
+			// }
 			?>
 
 
