8365675763b0384a4765a0e26e2288708f86b4c5
kaiyu li <krisfk@gmail.com>
2022-05-21 05:37:46 +0800

    .

--- wp-content/themes/musemum/footer.php
+++ wp-content/themes/musemum/footer.php
@@ -22,21 +22,8 @@
 
     <?php if ( has_nav_menu( 'footer' ) ) : ?>
     <nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation">
-        <ul class="footer-navigation-wrapper">
-            <?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'items_wrap'     => '%3$s',
-							'container'      => false,
-							'depth'          => 1,
-							'link_before'    => '<span>',
-							'link_after'     => '</span>',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-        </ul><!-- .footer-navigation-wrapper -->
+
+
     </nav><!-- .footer-navigation -->
     <?php endif; ?>
     <div class="site-info">
