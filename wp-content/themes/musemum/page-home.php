574b76988892f69b33cc93bb9694600c1604ef10
kaiyu li <krisfk@gmail.com>
2022-05-21 18:47:50 +0800

    .

--- wp-content/themes/musemum/page-home.php
+++ wp-content/themes/musemum/page-home.php
@@ -16,30 +16,41 @@ get_header();
 <div class="cal-pos-1"></div>
 <div class="cal-pos-2"></div>
 <div class="cal-pos-3"></div>
-<div classs="scene-1-bg">
 
-</div>
 <div class="main-container">
 
-    <img class="page-1-bg" class="h-100" src="http://64.227.13.14/musemum/wp-content/uploads/2022/05/page-1-bg.jpg"
-        alt="">
+    <!--scene-1 start-->
+
+    <div class="scene-div scene-div-1">
+        <div class="scene-1-bg">
+
+        </div>
 
 
-    <img class="white-big-mcd-logo h-100" class="h-100"
-        src="http://64.227.13.14/musemum/wp-content/uploads/2022/05/white-big-mcd-logo.png" alt="">
+        <img class="page-1-bg" class="h-100" src="http://64.227.13.14/musemum/wp-content/uploads/2022/05/page-1-bg.jpg"
+            alt="">
 
-    <img class="musemum-card" src="http://64.227.13.14/musemum/wp-content/uploads/2022/05/card.png" alt="">
 
-    <div class="scene-1-txt-1">
-        帶你體驗康文署博物館精彩的藝術 • 歷史 • <br>科學旅程！<br>
-        Enjoy the visit by inspiring experiences in Art • History • <br>
-        Science under the LCSD Museums
+        <img class="white-big-mcd-logo h-100" class="h-100"
+            src="http://64.227.13.14/musemum/wp-content/uploads/2022/05/white-big-mcd-logo.png" alt="">
+
+        <img class="musemum-card" src="http://64.227.13.14/musemum/wp-content/uploads/2022/05/card.png" alt="">
+
+        <div class="scene-1-txt-1">
+            帶你體驗康文署博物館精彩的藝術 • 歷史 • <br>科學旅程！<br>
+            Enjoy the visit by inspiring experiences in Art • History • <br>
+            Science under the LCSD Museums
+        </div>
+
+        <div class="scene-1-txt-2">
+            博物館<span class="big-font">通行證</span>辦理處 <br>
+            Museum Pass Counter
+        </div>
     </div>
 
-    <div class="scene-1-txt-2">
-        博物館<span class="big-font">通行證</span>辦理處 <br>
-        Museum Pass Counter
-    </div>
+    <!--scene-1 end-->
+
+
 
 
 </div>
