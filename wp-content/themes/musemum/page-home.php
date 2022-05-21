1a4354ada6a553cb0c71d5b10c64ac6692e71078
kaiyu li <krisfk@gmail.com>
2022-05-21 18:36:40 +0800

    .

--- wp-content/themes/musemum/page-home.php
+++ wp-content/themes/musemum/page-home.php
@@ -28,6 +28,13 @@ get_header();
 
     <img class="musemum-card" src="http://64.227.13.14/musemum/wp-content/uploads/2022/05/card.png" alt="">
 
+    <div class="scene-1-txt-1">
+        帶你體驗康文署博物館精彩的藝術 • 歷史 • <br>科學旅程！<br>
+        Enjoy the visit by inspiring experiences in Art • History • <br>
+        Science under the LCSD Museums
+    </div>
+
+</div>
 </div>
 <?php
 get_footer();
