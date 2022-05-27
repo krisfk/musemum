0eef35b263f37d3d11c8663d44ffbd23bd902637
kaiyu li <krisfk@gmail.com>
2022-05-27 11:55:25 +0800

    .

--- wp-content/themes/musemum/page-home.php
+++ wp-content/themes/musemum/page-home.php
@@ -92,7 +92,7 @@ get_header();
                 <source src="http://64.227.13.14/musemum/wp-content/uploads/2022/05/AR_UI.mp4" type="video/mp4">
             </video> -->
             <video class="iphone-video" controls>
-                <source src="http://64.227.13.14/musemum/wp-content/uploads/2022/05/Checkpoint11_UI.mp4"
+                <source src="http://64.227.13.14/musemum/wp-content/uploads/2022/05/Checkpoint11_UI_crop.mov"
                     type="video/mp4">
             </video>
 
