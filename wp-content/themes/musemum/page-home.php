3b2e4a4472edd846cef710b5d68410e278a0778f
kaiyu li <krisfk@gmail.com>
2022-05-27 19:15:33 +0800

    .

--- wp-content/themes/musemum/page-home.php
+++ wp-content/themes/musemum/page-home.php
@@ -204,7 +204,7 @@ $(function() {
 
 
                         var v_idx = 1;
-                        var total_video = $('.bg-behind-phone').length();
+                        var total_video = $('.bg-behind-phone').length;
                         $('.ar-videos-div .ar-video').on('ended',
                             function() {
 
