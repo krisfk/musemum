336df96951fad23b750b3b7b4951af7f6ca3cad2
kaiyu li <krisfk@gmail.com>
2022-05-29 05:37:51 +0800

    .

--- wp-content/themes/musemum/page-home.php
+++ wp-content/themes/musemum/page-home.php
@@ -936,14 +936,17 @@ $(function() {
             $('.weekday-weather-icon').fadeOut(0);
             $('.degree-div').fadeOut(0);
 
-            for (i = 0; i < $('.degree-div').length; i++) {
+            setTimeout(() => {
+                for (i = 0; i < $('.degree-div').length; i++) {
 
-                $('.weekday-title-div').eq(i).delay(100 + i * 30).slideDown(500);
-                $('.weekday-degree-div').eq(i).delay(700 + i * 30).slideDown(400);
-                $('.weekday-weather-icon').eq(i).delay(700 + i * 30).fadeIn(400)
-                $('.degree-div').eq(i).delay(700 + i * 30).fadeIn(400)
+                    $('.weekday-title-div').eq(i).delay(100 + i * 30).slideDown(500);
+                    $('.weekday-degree-div').eq(i).delay(700 + i * 30).slideDown(400);
+                    $('.weekday-weather-icon').eq(i).delay(700 + i * 30).fadeIn(400)
+                    $('.degree-div').eq(i).delay(700 + i * 30).fadeIn(400)
 
-            }
+                }
+            }, 5000);
+
 
 
 
