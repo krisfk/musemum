799e35479f05e283dfadf748321ad45482aac7d5
kaiyu li <krisfk@gmail.com>
2022-06-01 15:16:23 +0800

    .

--- wp-content/themes/musemum/page-home.php
+++ wp-content/themes/musemum/page-home.php
@@ -908,6 +908,7 @@ $(function() {
                 var current_temperature = result.temperature.data[1].value;
                 var current_humidity = result.humidity.data.value;
                 console.log(result.humidity);
+                console.log(55);
 
                 $('.today-weather-icon').attr('src', site_url + '/weather-icon/pic' +
                     today_weather_icon_idx + '.png');
