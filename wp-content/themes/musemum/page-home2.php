af7c4456b3f3611051f817bd7b79e3c9a78c3e82
kaiyu li <krisfk@gmail.com>
2022-08-19 04:54:47 +0800

    .

--- wp-content/themes/musemum/page-home2.php
+++ wp-content/themes/musemum/page-home2.php
@@ -690,7 +690,7 @@
                             <div class="scene-4-screen scene-4-screen-2">
 
                                 <div class="weather-center-blk">
-                                    <div class="weather-place">筲箕灣 <br> Shau Kei Wan </div>
+                                    <div class="weather-place">筲箕灣 Shau Kei Wan </div>
 
                                     <div class="scene-4-screen-2-block scene-4-screen-2-block-1">
                                         <div class="weather-msg"> <img class="weather-icon today-weather-icon" src=""
