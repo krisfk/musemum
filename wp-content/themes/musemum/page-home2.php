6e7bf630d9e40f7449a5095e04a8c519effaa7d9
kaiyu li <krisfk@gmail.com>
2022-08-18 18:37:16 +0800

    .

--- wp-content/themes/musemum/page-home2.php
+++ wp-content/themes/musemum/page-home2.php
@@ -290,19 +290,9 @@
                                             </div>
                                         </div>
                                         <div class="event-content-div" style="background:#dfdfdf">
-                                            <div> <span style="font-size: 55px;">同暢遊 齊共賞</span><br> <span
-                                                    style="font-size: 45px;">Enjoy the
-                                                    visit by inspiring experiences in Art • History • Science under the
-                                                    LCSD Museums </span>
-                                                <br><br><br> <span style="font-size: 70px;">11 :00</span><span
-                                                    style="font-size:35px;margin-left:5px;">AM</span>
-                                                <span
-                                                    style="font-size: 50px; margin: 0 10px 0 10px; position: relative; top: -10px;">|</span>
-                                                <span style="font-size:70px;">15:00</span> <span
-                                                    style="font-size:35px;margin-left:5px;">PM</span> </span>
-                                                <br><br><br> <span style="font-size: 45px;">逢周末及公眾假期 <br> Weekends &amp;
-                                                    Public
-                                                    Holidays</span>
+                                            <div>
+                                                <div>09:00 - 13:00AM / <br>
+                                                    18:00 - 20:00PM</div>
                                             </div>
                                         </div>
                                     </div>
