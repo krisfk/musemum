f040c8cd715a66e7c3eea6000e3356f366e007d1
kaiyu li <krisfk@gmail.com>
2022-08-18 19:15:41 +0800

    .

--- wp-content/themes/musemum/page-home2.php
+++ wp-content/themes/musemum/page-home2.php
@@ -438,7 +438,8 @@
                         <!--scene-3 start-->
                         <div class="scene-div scene-div-3">
 
-                            <div class="scene-3-screen scene-3-screen-1">
+                            <div class="scene-3-screen scene-3-screen-1"
+                                style="background:url(http://64.227.13.14/musemum/wp-content/uploads/2022/08/little-plant.jpg);">
                                 <!-- http://64.227.13.14/musemum/wp-content/uploads/2022/08/little-plant.jpg -->
                                 <!-- <img class="scene-3-screen-left-img"
                                     src="http://64.227.13.14/musemum/wp-content/uploads/2022/05/publication-left-img-1.jpg"
