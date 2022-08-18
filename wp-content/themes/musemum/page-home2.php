827dbb96a642adf988ef2c59ce0fce49f6bc6386
kaiyu li <krisfk@gmail.com>
2022-08-18 18:52:02 +0800

    .

--- wp-content/themes/musemum/page-home2.php
+++ wp-content/themes/musemum/page-home2.php
@@ -283,87 +283,48 @@
                                 <div class="scene-2-screen-2-slide">
                                     <div class="event-div one-col-width">
                                         <div class="event-title-div" style="background:#394d64;">
-                                            <div>
-                                                <span style="font-size: 60px;">免費公眾導賞</span> <br>
-                                                <span style="font-size: 50px; font-weight:bold">Free Public Guide
-                                                    Tours</span>
-                                            </div>
+                                            <div> <span style="font-size: 60px;">免費公眾導賞</span> <br> <span
+                                                    style="font-size: 50px; font-weight:bold">Free Public Guide
+                                                    Tours</span> </div>
                                         </div>
                                         <div class="event-content-div" style="background:#fff">
-                                            <div style="
-    width: 530px;
-">
+                                            <div style=" width: 530px; ">
                                                 <div style="font-size: 65px;font-weight: bold;">09:00 - 13:00<span
-                                                        style="
-    font-size: 32px;
-">AM/</span> <br>
-                                                    18:00 - 20:00<span style="
-    font-size: 32px;
-">PM</span></div>
+                                                        style=" font-size: 32px; ">AM/</span> <br> 18:00 - 20:00<span
+                                                        style=" font-size: 32px; ">PM</span></div>
                                                 <div style="font-size: 28px;color: #808080;margin: 20px 0 0 0;"
-                                                    class="">
-                                                    逢周末及公眾假期 <br>
-                                                    Weekends &amp; Public Holidays
-                                                </div>
-
+                                                    class=""> 逢周末及公眾假期 <br> Weekends &amp; Public Holidays </div>
                                                 <div style="height: 6px;/* width: 545px; */background:#000;margin: 25px 0 0 0;"
-                                                    class="">
+                                                    class=""> </div>
+                                                <div class="" style=" font-size: 30px; margin: 39px 0 0 0; "> 同暢遊 齊共賞
+                                                    <br> Enjoy the visit by inspiring experiences in Art • History •
+                                                    Science under the LCSD Museums
                                                 </div>
-                                                <div class="" style="
-    font-size: 30px;
-    margin: 39px 0 0 0;
-">
-                                                    同暢遊 齊共賞 <br>
-                                                    Enjoy the visit by inspiring experiences in Art • History • Science
-                                                    under the LCSD Museums
-                                                </div>
-
                                             </div>
                                         </div>
                                     </div>
-
                                     <div class="event-div one-col-width">
                                         <div class="event-title-div" style="background:#394d64;">
-                                            <div>
-                                                <span style="font-size: 60px;">免費公眾導賞</span> <br>
-                                                <span style="font-size: 50px; font-weight:bold">Free Public Guide
-                                                    Tours</span>
+                                            <div> <span style="font-size: 60px;">探索地帶 </span> <br> <span
+                                                    style="font-size: 50px; font-weight:bold">Discovery Zone</span>
                                             </div>
                                         </div>
                                         <div class="event-content-div" style="background:#fff">
-                                            <div style="
-    width: 530px;
-">
-                                                <div style="font-size: 65px;font-weight: bold;">09:00 - 13:00<span
-                                                        style="
-    font-size: 32px;
-">AM/</span> <br>
-                                                    18:00 - 20:00<span style="
-    font-size: 32px;
-">PM</span></div>
+                                            <div style=" width: 530px; ">
+                                                <div style="font-size: 65px;font-weight: bold;">10:00<span
+                                                        style=" font-size: 32px; ">AM-</span> <br> 15:00<span
+                                                        style=" font-size: 32px; ">PM</span></div>
                                                 <div style="font-size: 28px;color: #808080;margin: 20px 0 0 0;"
-                                                    class="">
-                                                    逢周末及公眾假期 <br>
-                                                    Weekends &amp; Public Holidays
-                                                </div>
-
+                                                    class=""> 逢周末及公眾假期 <br> Weekends &amp; Public Holidays </div>
                                                 <div style="height: 6px;/* width: 545px; */background:#000;margin: 25px 0 0 0;"
-                                                    class="">
+                                                    class=""> </div>
+                                                <div class="" style=" font-size: 30px; margin: 39px 0 0 0; "> 嘗探索、享體驗
+                                                    <br>
+                                                    To Explore, To Learnz
                                                 </div>
-                                                <div class="" style="
-    font-size: 30px;
-    margin: 39px 0 0 0;
-">
-                                                    同暢遊 齊共賞 <br>
-                                                    Enjoy the visit by inspiring experiences in Art • History • Science
-                                                    under the LCSD Museums
-                                                </div>
-
                                             </div>
                                         </div>
                                     </div>
-
-
                                 </div>
                                 <div class="scene-2-screen-2-slide">
                                     <div class="event-div one-col-width">
