42ed61314f42ec3398589324ecaf85a3e82e847e
kaiyu li <krisfk@gmail.com>
2022-08-19 09:43:59 +0800

    .

--- wp-content/themes/musemum/page-home2.php
+++ wp-content/themes/musemum/page-home2.php
@@ -780,9 +780,11 @@ date_default_timezone_set('Asia/Hong_Kong');
 
                                 </table>
 
-                                <div class="lantern-div">因惡劣天氣影響，史蹟徑暫時關閉 Due to the warning, Historic Trail is
-                                    temporarily
-                                    closed
+                                <div class="lantern-div">
+
+                                    <marquee>因惡劣天氣影響，史蹟徑暫時關閉 Due to the warning, Historic Trail is
+                                        temporarily
+                                        closed</marquee>
                                 </div>
 
 
