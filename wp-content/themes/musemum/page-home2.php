56b19e546085bc0b26f2f6a0d4b3e802ee146f20
kaiyu li <krisfk@gmail.com>
2022-08-19 09:31:08 +0800

    .

--- wp-content/themes/musemum/page-home2.php
+++ wp-content/themes/musemum/page-home2.php
@@ -1308,10 +1308,10 @@ function cndate($date){
                             $.get("https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=warnsum&lang=tc",
                                 function(result) {
 
-                                    var result =
-                                        JSON.parse(
-                                            '{"WTS":{"name":"雷暴警告","code":"WTS","actionCode":"EXTEND","issueTime":"2022-08-09T06:30:00+08:00","expireTime":"2022-08-09T21:30:00+08:00","updateTime":"2022-08-09T19:20:00+08:00"},"WTCSGNL":{"name":"熱帶氣旋警告信號","code":"TC3","actionCode":"ISSUE","type":"三號強風信號","issueTime":"2022-08-09T11:25:00+08:00","updateTime":"2022-08-09T11:25:00+08:00"},"WFIRE": { "name": "火災危險警告", "code": "WFIRER", "type": "紅色", "actionCode": "ISSUE", "issueTime": "2020-09-24T11:15:00+08:00", "updateTime": "2020-09-24T11:15:00+08:00" }}'
-                                        );
+                                    // var result =
+                                    //     JSON.parse(
+                                    //         '{"WTS":{"name":"雷暴警告","code":"WTS","actionCode":"EXTEND","issueTime":"2022-08-09T06:30:00+08:00","expireTime":"2022-08-09T21:30:00+08:00","updateTime":"2022-08-09T19:20:00+08:00"},"WTCSGNL":{"name":"熱帶氣旋警告信號","code":"TC3","actionCode":"ISSUE","type":"三號強風信號","issueTime":"2022-08-09T11:25:00+08:00","updateTime":"2022-08-09T11:25:00+08:00"},"WFIRE": { "name": "火災危險警告", "code": "WFIRER", "type": "紅色", "actionCode": "ISSUE", "issueTime": "2020-09-24T11:15:00+08:00", "updateTime": "2020-09-24T11:15:00+08:00" }}'
+                                    //     );
 
                                     var warning_arr = Object.keys(result);
                                     // console.log(result);
