b64d2f2424c3525c648189273b92f83900523ff1
kaiyu li <krisfk@gmail.com>
2022-11-20 23:16:25 +0800

    .

--- wp-content/themes/musemum/page-home3.php
+++ wp-content/themes/musemum/page-home3.php
@@ -34,11 +34,7 @@ date_default_timezone_set('Asia/Hong_Kong');
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <?php //wp_head(); ?>
-    <link rel='stylesheet' id='twenty-twenty-one-style-css'
-        href='http://64.227.13.14/musemum/wp-content/themes/musemum/style-old-2.css?ver=1.5' media='all' />
-
-
+    <?php wp_head(); ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- <meta name="viewport" content="width=100, initial-scale=1.0"> -->
