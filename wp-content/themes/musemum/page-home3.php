3e434b69135f5d2cca66e8f6897062a5b6a8a9c4
kaiyu li <krisfk@gmail.com>
2022-11-21 00:59:57 +0800

    .

--- wp-content/themes/musemum/page-home3.php
+++ wp-content/themes/musemum/page-home3.php
@@ -466,17 +466,21 @@ date_default_timezone_set('Asia/Hong_Kong');
                                         </span>
 
                                     </div>
-                                    <table>
-                                        <tr>
-                                            <td style="color:#808080;" class="pe-3"> <span style="font-size:20px;">
-                                                    港幣 <br> HK
-                                                </span></td>
-                                            <td style="color:#35475d;font-weight:bold;position: relative;top: -1px;">
-                                                <span style="font-size:50px;">$100</span>
-                                            </td>
-                                        </tr>
-                                    </table>
 
+                                    <div class="publication-div-text-content publication-div-text-content-price">
+                                        <table>
+                                            <tr>
+                                                <td style="color:#808080;" class="pe-3"> <span style="font-size:20px;">
+                                                        港幣 <br> HK
+                                                    </span></td>
+                                                <td
+                                                    style="color:#35475d;font-weight:bold;position: relative;top: -1px;">
+                                                    <span style="font-size:50px;">$100</span>
+                                                </td>
+                                            </tr>
+                                        </table>
+
+                                    </div>
                                 </div>
 
                             </div>
